1eabf67967b64f45f3432fc97470bbb699271456
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-12 21:47:08 +0100

    manager test

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../lib/Manager");
+
+test("Add a new manager", () => {
+  const manager = new Manager("Mary", 456, "mary456@yahoo.com", 001);
+
+  expect(manager.name).toBe("Mary");
+  expect(manager.id).toEqual(expect.any(Number));
+  expect(manager.email).toEqual(expect.any(String));
+  expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test("Check all methods for Manager class", () => {
+  const manager = new Manager("Mary", 456, "mary456@yahoo.com", 001);
+
+  expect(manager.getName()).toBe(manager.name);
+  expect(manager.getId()).toBe(manager.id);
+  expect(manager.getEmail()).toBe(manager.email);
+  expect(manager.getOfficeNumber()).toBe(manager.officeNumber);
+  expect(manager.getRole()).toBe("Manager");
+});
