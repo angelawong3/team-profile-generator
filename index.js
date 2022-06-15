7fce3440c8c6e8fb690f0452fcba7f95bf637eee
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-15 20:11:09 +0100

    prompt q for engineer

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// inquirer package
 const inquirer = require("inquirer");
 
+// import questions
 const { Manager, managerInfoArray } = require("./lib/Manager");
 const { Engineer, engineerInfoArray } = require("./lib/Engineer");
 const { Intern, internInfoArray } = require("./lib/Intern");
+
+// empty array for pushing answers
 const employeesArray = [];
 
-const writeFile = require("./src/writeFile");
-const templateHTML = require("./src/html");
+// import writeFile and the temp HTML
+const { writeFile } = require("./src/writeFile");
+const { templateHTML } = require("./src/html");
 
 // prompt questions for manager
 const managerQuestions = () => {
@@ -16,6 +21,20 @@ const managerQuestions = () => {
       answers.id,
       answers.email,
       answers.officeNumber
+    );
+    employeesArray.push(answers);
+    return employeePrompt();
+  });
+};
+
+// prompt questions for engineer
+const engineerQuestions = () => {
+  inquirer.prompt(engineerInfoArray).then((answers) => {
+    answers = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
     );
     employeesArray.push(answers);
     return employeePrompt();
