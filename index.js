445a4d5582d76ecdefaa5bf8e170b67443059e39
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-15 20:06:32 +0100

    prompt q for manager

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const inquirer = require("inquirer");
-const writeFile = require("./src/writeFile");
-const templateHTML = require("./src/html");
 
 const { Manager, managerInfoArray } = require("./lib/Manager");
 const { Engineer, engineerInfoArray } = require("./lib/Engineer");
 const { Intern, internInfoArray } = require("./lib/Intern");
 const employeesArray = [];
+
+const writeFile = require("./src/writeFile");
+const templateHTML = require("./src/html");
+
+// prompt questions for manager
+const managerQuestions = () => {
+  inquirer.prompt(managerInfoArray).then((answers) => {
+    answers = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.officeNumber
+    );
+    employeesArray.push(answers);
+    return employeePrompt();
+  });
+};
