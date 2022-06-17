3516716cc049d8193cfc27c93989c2deb7068a6a
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-17 16:21:22 +0100

    manager test pass

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,20 +1,26 @@
 const { Manager } = require("../lib/Manager");
 
-test("Add a new manager", () => {
-  const manager = new Manager("Mary", 456, "mary456@yahoo.com", 789);
+describe("Manager", () => {
+  describe("Add a new manager", () => {
+    it("name should be mary, id should be number, email should be string, office number should be number", () => {
+      const manager = new Manager("Mary", 456, "mary456@yahoo.com", 789);
 
-  expect(manager.name).toBe("Mary");
-  expect(manager.id).toEqual(expect.any(Number));
-  expect(manager.email).toEqual(expect.any(String));
-  expect(manager.officeNumber).toEqual(expect.any(Number));
-});
+      expect(manager.name).toBe("Mary");
+      expect(manager.id).toEqual(expect.any(Number));
+      expect(manager.email).toEqual(expect.any(String));
+      expect(manager.officeNumber).toEqual(expect.any(Number));
+    });
+  });
 
-test("Check all methods for Manager class", () => {
-  const manager = new Manager("Mary", 456, "mary456@yahoo.com", 789);
+  describe("Check all methods for Manager class", () => {
+    it("should create name, id, email, office number and role", () => {
+      const manager = new Manager("Mary", 456, "mary456@yahoo.com", 789);
 
-  expect(manager.getName()).toBe(manager.name);
-  expect(manager.getId()).toBe(manager.id);
-  expect(manager.getEmail()).toBe(manager.email);
-  expect(manager.getOfficeNumber()).toBe(manager.officeNumber);
-  expect(manager.getRole()).toBe("Manager");
+      expect(manager.getName()).toBe(manager.name);
+      expect(manager.getId()).toBe(manager.id);
+      expect(manager.getEmail()).toBe(manager.email);
+      expect(manager.getOfficeNumber()).toBe(manager.officeNumber);
+      expect(manager.getRole()).toBe("Manager");
+    });
+  });
 });
