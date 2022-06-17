96e851cb3b5676e0b84d935077b84335a1c11ccd
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-17 16:12:35 +0100

    employee test pass

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,18 +1,24 @@
 const Employee = require("../lib/Employee");
 
-test("Add a new employee", () => {
-  const employee = new Employee("Tom", 123, "tom123@yahoo.com");
+describe("Employee", () => {
+  describe("Add a new employee", () => {
+    it("name should be tom, id should be number, email should be string", () => {
+      const employee = new Employee("Tom", 123, "tom123@yahoo.com");
 
-  expect(employee.name).toBe("Tom");
-  expect(employee.id).toEqual(expect.any(Number));
-  expect(employee.email).toEqual(expect.any(String));
-});
+      expect(employee.name).toBe("Tom");
+      expect(employee.id).toEqual(expect.any(Number));
+      expect(employee.email).toEqual(expect.any(String));
+    });
+  });
 
-test("Check all methods for Employee class", () => {
-  const employee = new Employee("Tom", 123, "tom123@yahoo.com");
+  describe("Check all methods for Employee class", () => {
+    it("should create name, id, email and role", () => {
+      const employee = new Employee("Tom", 123, "tom123@yahoo.com");
 
-  expect(employee.getName()).toBe(employee.name);
-  expect(employee.getId()).toBe(employee.id);
-  expect(employee.getEmail()).toBe(employee.email);
-  expect(employee.getRole()).toBe("Employee");
+      expect(employee.getName()).toBe(employee.name);
+      expect(employee.getId()).toBe(employee.id);
+      expect(employee.getEmail()).toBe(employee.email);
+      expect(employee.getRole()).toBe("Employee");
+    });
+  });
 });
