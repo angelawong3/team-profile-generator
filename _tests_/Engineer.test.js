3cd3532f09e3be59f48ee557603c7ade6f655ebe
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-17 16:23:19 +0100

    enginner test pass

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,20 +1,36 @@
 const { Engineer } = require("../lib/Engineer");
 
-test("Add a new engineer", () => {
-  const engineer = new Engineer("Peter", 234, "peter234@yahoo.com", "peter234");
+describe("Engineer", () => {
+  describe("Add a new engineer", () => {
+    it("name should be peter, id should be number, email should be string, github should be string", () => {
+      const engineer = new Engineer(
+        "Peter",
+        234,
+        "peter234@yahoo.com",
+        "peter234"
+      );
 
-  expect(engineer.name).toBe("Peter");
-  expect(engineer.id).toEqual(expect.any(Number));
-  expect(engineer.email).toEqual(expect.any(String));
-  expect(engineer.github).toEqual(expect.any(String));
-});
+      expect(engineer.name).toBe("Peter");
+      expect(engineer.id).toEqual(expect.any(Number));
+      expect(engineer.email).toEqual(expect.any(String));
+      expect(engineer.github).toEqual(expect.any(String));
+    });
+  });
 
-test("Check all methods for Engineer class", () => {
-  const engineer = new Engineer("Peter", 234, "peter234@yahoo.com", "peter234");
+  describe("Check all methods for Engineer class", () => {
+    it("should create name, id, email, github and role", () => {
+      const engineer = new Engineer(
+        "Peter",
+        234,
+        "peter234@yahoo.com",
+        "peter234"
+      );
 
-  expect(engineer.getName()).toBe(engineer.name);
-  expect(engineer.getId()).toBe(engineer.id);
-  expect(engineer.getEmail()).toBe(engineer.email);
-  expect(engineer.getGithub()).toBe(engineer.github);
-  expect(engineer.getRole()).toBe("Engineer");
+      expect(engineer.getName()).toBe(engineer.name);
+      expect(engineer.getId()).toBe(engineer.id);
+      expect(engineer.getEmail()).toBe(engineer.email);
+      expect(engineer.getGithub()).toBe(engineer.github);
+      expect(engineer.getRole()).toBe("Engineer");
+    });
+  });
 });
