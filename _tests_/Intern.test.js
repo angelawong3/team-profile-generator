60d47c7613671c13b717281902b7a828a9ae6f6f
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-17 16:24:54 +0100

    intern test pass

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,30 +1,36 @@
 const { Intern } = require("../lib/Intern");
 
-test("Add a new intern", () => {
-  const intern = new Intern(
-    "Betty",
-    567,
-    "betty567@yahoo.com",
-    "University of Birmingham"
-  );
+describe("Intern", () => {
+  describe("Add a new intern", () => {
+    it("name should be betty, id should be number, email should be string, school should be string", () => {
+      const intern = new Intern(
+        "Betty",
+        567,
+        "betty567@yahoo.com",
+        "University of Birmingham"
+      );
 
-  expect(intern.name).toBe("Betty");
-  expect(intern.id).toEqual(expect.any(Number));
-  expect(intern.email).toEqual(expect.any(String));
-  expect(intern.school).toEqual(expect.any(String));
-});
+      expect(intern.name).toBe("Betty");
+      expect(intern.id).toEqual(expect.any(Number));
+      expect(intern.email).toEqual(expect.any(String));
+      expect(intern.school).toEqual(expect.any(String));
+    });
+  });
 
-test("Check all methods for Intern class", () => {
-  const intern = new Intern(
-    "Betty",
-    567,
-    "betty567@yahoo.com",
-    "University of Birmingham"
-  );
+  describe("Check all methods for Intern class", () => {
+    it("should create name, id, email, school and role", () => {
+      const intern = new Intern(
+        "Betty",
+        567,
+        "betty567@yahoo.com",
+        "University of Birmingham"
+      );
 
-  expect(intern.getName()).toBe(intern.name);
-  expect(intern.getId()).toBe(intern.id);
-  expect(intern.getEmail()).toBe(intern.email);
-  expect(intern.getSchool()).toBe(intern.school);
-  expect(intern.getRole()).toBe("Intern");
+      expect(intern.getName()).toBe(intern.name);
+      expect(intern.getId()).toBe(intern.id);
+      expect(intern.getEmail()).toBe(intern.email);
+      expect(intern.getSchool()).toBe(intern.school);
+      expect(intern.getRole()).toBe("Intern");
+    });
+  });
 });
